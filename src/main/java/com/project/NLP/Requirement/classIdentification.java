1f2d6cfe5052119bd6f7bd60db1cd7d7e9146cc1
shobi22 <shobi.11@cse.mrt.ac.lk>
2015-07-30 01:54:31 +0530

    added rule2 - eliminating design elements from class and attributes

--- src/main/java/com/project/NLP/Requirement/classIdentification.java
+++ src/main/java/com/project/NLP/Requirement/classIdentification.java
@@ -18,10 +18,11 @@ public class classIdentification {
     private ArrayList priorToRule1;
     private ArrayList classList;
     private ArrayList afterRule1 = new ArrayList();
-    
+
     private phrasesIdentification np;
     private ArrayList attributeFromClass = new ArrayList();
     private designElementClass designEleClass = new designElementClass();
+
     classIdentification() {
 
     }
@@ -66,30 +67,26 @@ public class classIdentification {
             innerPhraseList = pI.getIdentifiedPhrases(p);
             if (innerPhraseList.size() == 2) {
                 /*checks whether the list contains the design elements
-                if the list don't have the design elements then add it to 
-                the afterRule1 and attributeFromClass
-                */
-                checksWhetherDesignElementExits(innerPhraseList.get(0),innerPhraseList.get(1) );
-                //if(!isContainDesignElements(innerPhraseList)){
-                    /*first noun phrase is considered as class*/
-                    //afterRule1.add(innerPhraseList.get(0));
-                    /*second noun phrase is considered as attribute*/
-                    //attributeFromClass.add(innerPhraseList.get(1));
-               // }
-                
+                 if the list don't have the design elements then add it to 
+                 the afterRule1 and attributeFromClass
+                 */
+                checksWhetherDesignElementExits(innerPhraseList.get(0), innerPhraseList.get(1));
+
+                /*first noun phrase is considered as class*/
+                //afterRule1.add(innerPhraseList.get(0));
+                /*second noun phrase is considered as attribute*/
+                //attributeFromClass.add(innerPhraseList.get(1));
             } 
             /*rule 1 but if the the attribute is with the pronoun
-            the last NP is taken as the attribute and 
-            before the last element is taken as the class
-            */
-            else if(innerPhraseList.size()>2){
-                checksWhetherDesignElementExits(innerPhraseList.get((innerPhraseList.size()-2)),innerPhraseList.get((innerPhraseList.size()-1)) );
-                
+             the last NP is taken as the attribute and 
+             before the last element is taken as the class
+             */ 
+            else if (innerPhraseList.size() > 2) {
+                checksWhetherDesignElementExits(innerPhraseList.get((innerPhraseList.size() - 2)), innerPhraseList.get((innerPhraseList.size() - 1)));
+
                 //afterRule1.add(innerPhraseList.get((innerPhraseList.size()-2)));
                 //attributeFromClass.add(innerPhraseList.get((innerPhraseList.size()-1)));
-                
-            }   
-            else if (innerPhraseList.size() == 1) {
+            } else if (innerPhraseList.size() == 1) {
                 System.out.println("working...");
                 //checkPRP(pI);
                 //innerPhraseList.add(priorToRule1.get(0));
@@ -100,49 +97,49 @@ public class classIdentification {
     }
 
     /*this method checks whether the attribute or class contains the design elemets.
-    if so it will skip those elements and store rest of the artefacts
-    parameter: className, attribute
-    */
-    public void checksWhetherDesignElementExits(Object className, Object attribute){
+     if so it will skip those elements and store rest of the artefacts
+     parameter: className, attribute
+     */
+    public void checksWhetherDesignElementExits(Object className, Object attribute) {
         ArrayList designElements = designEleClass.getDesignElementsList();
         //for(int i=0;i<designElements.size();i++){
-            
-            if(!designElements.contains(className)){
-                System.out.println("design class... :"+String.valueOf(className) );
-                afterRule1.add(String.valueOf(className));
-                
-            }
-            if(!designElements.contains(attribute)){
-                System.out.println("design attribute... :"+String.valueOf(attribute) );
-                attributeFromClass.add(String.valueOf(attribute));
-                
-            }
-            
-       // }
+
+        if (!designElements.contains(className)) {
+            System.out.println("design class... :" + String.valueOf(className));
+            afterRule1.add(String.valueOf(className));
+
+        }
+        if (!designElements.contains(attribute)) {
+            System.out.println("design attribute... :" + String.valueOf(attribute));
+            attributeFromClass.add(String.valueOf(attribute));
+
+        }
+
+        // }
     }
-    
+
     /*rule1 continuos...
-    if the phrase consists PRP + NN identify the PRP as a class and NN as attribute
-    but the PRP is stored already in class list. so store the NN as attributeFromClass
-    */
-    private void checkPRP(phrasesIdentification PI){
+     if the phrase consists PRP + NN identify the PRP as a class and NN as attribute
+     but the PRP is stored already in class list. so store the NN as attributeFromClass
+     */
+    private void checkPRP(phrasesIdentification PI) {
         ArrayList innerPhraseList = PI.getIdentifiedPhrases("NP");
         //ArrayList 
-        
+
         ArrayList nn;
         //Boolean status= false;
-        
-        for(int i=0;i<innerPhraseList.size();i++){
-            System.out.println("prp.... :"+innerPhraseList);
-            if(innerPhraseList.get(i).equals("PRP")){
-                System.out.println("prp.... :"+innerPhraseList.get(i));
-                attributeFromClass.add(innerPhraseList.get(i+1));
+
+        for (int i = 0; i < innerPhraseList.size(); i++) {
+            System.out.println("prp.... :" + innerPhraseList);
+            if (innerPhraseList.get(i).equals("PRP")) {
+                System.out.println("prp.... :" + innerPhraseList.get(i));
+                attributeFromClass.add(innerPhraseList.get(i + 1));
                 break;
             }
         }
 
     }
-    
+
     public ArrayList innerPhrases1(String p) {
 
         ArrayList innerPhraseList = new ArrayList();
