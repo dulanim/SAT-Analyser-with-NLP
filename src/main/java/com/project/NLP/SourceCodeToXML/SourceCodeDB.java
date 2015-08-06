702bf90745e287b49985f04c5c780974f112c362
aarthika-nathan <aarthinathan15@gmail.com>
2015-08-06 21:06:20 +0530

    Operations on Database

--- src/main/java/com/project/NLP/SourceCodeToXML/SourceCodeDB.java
+++ src/main/java/com/project/NLP/SourceCodeToXML/SourceCodeDB.java
@@ -1,16 +1,22 @@
 package com.project.NLP.SourceCodeToXML;
 
+import com.project.traceability.common.PropertyFile;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.helpers.collection.IteratorUtil;
+import org.neo4j.helpers.collection.MapUtil;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -27,70 +33,98 @@ public class SourceCodeDB {
 
         EXTENDS
     }
-    GraphDatabaseService graphDb;
-    Node firstNode;
-    Node secondNode;
-    Relationship relationship;
+
+    public static enum ClassLabel implements Label {
+
+        CLASS_NODE
+    }
+    private static GraphDatabaseService graphDb;
+    private static Node classNode;
+    private static Node superClassNode;
+    private static Relationship relationship;
 
     public SourceCodeDB() {
         //start the database server
-        graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("D:\\f1234.graphdb");
-        // registerShutdownHook(graphDb);
+        graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(PropertyFile.filePath + "\\test6.graphdb");
+        registerShutdownHook(graphDb);
     }
 
-    public void start() {
+    public static void createNodes() {
+        classNode = graphDb.createNode(ClassLabel.CLASS_NODE);
+        superClassNode = graphDb.createNode(ClassLabel.CLASS_NODE);
+        relationship = classNode.createRelationshipTo(superClassNode, RelTypes.EXTENDS);
+    }
+
+    public void addRelationshipData(String class1, String id1, String class2) {
         try (Transaction tx = graphDb.beginTx()) {
-            // Database operations go here
-            firstNode = graphDb.createNode();
-            firstNode.addLabel(DynamicLabel.label( "Node1" ));
-            firstNode.setProperty("message", "Hello");
-            firstNode.setProperty("value", "abc");
-            secondNode = graphDb.createNode();
-            secondNode.addLabel(DynamicLabel.label( "Node2" ));
-            secondNode.setProperty("message", "World!");
-
-            relationship = firstNode.createRelationshipTo(secondNode, RelTypes.EXTENDS);
-            relationship.setProperty("message", "brave Neo4j ");
+            // Perform DB operations			
+            createNodes();
+            classNode.setProperty("className", class1);
+            classNode.setProperty("class_id", id1);
+            if (!class2.isEmpty()) {
+                superClassNode.setProperty("superClassName", class2);
+            }
             tx.success();
-
-            System.out.print(firstNode.getProperty("message"));
-            System.out.print(relationship.getProperty("message"));
-            System.out.print(secondNode.getProperty("message"));
-
         }
     }
 
-    public void query() {
-        //graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("D:\\SourceCode.graphdb");
+    public void getRelationshipData() {
+        //Identify the relationship class and get the id
         ExecutionEngine execEngine = new ExecutionEngine(graphDb);
-        ExecutionResult execResult = execEngine.execute("MATCH (node1:Node1),(node2:Node2) \n"
-                + "WHERE node1.message = \"Hello\" \n"
+        ExecutionResult execResult = execEngine.execute("MATCH (node1:CLASS_NODE),(node2:CLASS_NODE) \n"
                 + "CREATE (node1)-[r:EXTENDS]->(node2) \n"
-                + "RETURN r,node2");
+                + "RETURN node1,node2");
         String results = execResult.dumpToString();
+        //System.out.println("Result\n"+results);
 
-        while (execResult.iterator().hasNext()) {
+        /*while (execResult.iterator().hasNext()) {
             Map<String, Object> row = execResult.iterator().next();
             for (Entry<String, Object> column : row.entrySet()) {
-                System.out.println("Row"+column.getKey() + ": " + column.getValue() + "; ");
+                System.out.println("Row" + column.getKey() + ": " + column.getValue() + "; ");
             }
+        }*/
+    }
+
+    public void getClassID(String className) {
+        String classID = "";
+        ExecutionEngine execEngine = new ExecutionEngine(graphDb);
+        ExecutionResult execResult = execEngine.execute("MATCH (node:CLASS_NODE) \n"
+                + "WHERE node.className={className} \n"
+                + "RETURN node.class_id,node", MapUtil.map("className", className));
+        String results = execResult.dumpToString();
+                                    
+        Iterator<Node> n_column = execResult.columnAs("node");
+        for (Node node : IteratorUtil.asIterable(n_column)) {
+            classID = node+":"+node.getProperty("class_id");
         }
 
-        /*
-         MATCH (cust:Customer),(cc:CreditCard) 
-         WHERE cust.id = "1001" AND cc.id= "5001" 
-         CREATE (cust)-[r:DO_SHOPPING_WITH{shopdate:"12/12/2014",price:55000}]->(cc) 
-         RETURN r
-
-         */
+        System.out.println("ID - "+classID);
         System.out.println(results);
+    }
 
+    private static void registerShutdownHook(final GraphDatabaseService graphDb) {
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                graphDb.shutdown();
+            }
+        });
     }
 
     public static void main(String[] args) {
         SourceCodeDB scdb = new SourceCodeDB();
-        scdb.start();
-        scdb.query();
+        scdb.addRelationshipData("Student", "SC1", "Person");
+        scdb.addRelationshipData("Person", "SC2", "");
+        scdb.addRelationshipData("Employee", "SC3", "Person");
+        scdb.addRelationshipData("Train", "SC4", "");
+        System.out.println("Added Data");
+        scdb.getRelationshipData();
+        System.out.println("Got Data");
+        scdb.getClassID("Train");
+        System.out.println("Got Data ID");
+        registerShutdownHook(graphDb);
+
     }
 
 }
