d64e121b1458475a09c5ed79088b22210994af99
shiyamalan <rsrishiya@gmail.com>
2015-12-30 18:24:44 +0530

    shiyamalan added some variable

--- src/main/java/com/project/NLP/file/operations/FilePropertyName.java
+++ src/main/java/com/project/NLP/file/operations/FilePropertyName.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.project.NLP.file.operations;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-import com.project.traceability.GUI.HomeGUI;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -15,8 +10,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import org.eclipse.swt.widgets.DirectoryDialog;
+
 import org.eclipse.swt.widgets.FileDialog;
 
 
@@ -44,22 +38,23 @@ public class FilePropertyName {
     public static final String UML_ARTIFACT_NAME = "UMLArtefactFile.xml";
     public static final String REQUIREMENT_ARTIFACT_NAME = "RequirementArtefactFile.xml";
     public static final String SOURCE_ARTIFACT_NAME = "SourceCodeArtefactFile.xml";
-    public static final String IMAGE_PATH = System.getProperty("user.dir") + File.separator + "img"+ File.separator;
-    public static Path getPath(Path target,String type){
-        if(type.contains(XML)){
-            target = Paths.get(HomeGUI.projectPath + File.separator + XML);
-        }else if(type.contains(REQUIREMENT) || type.contains("txt") || type.contains("pdf")||
-                type.contains("docs")||type.contains("doc")){
-            target = Paths.get(HomeGUI.projectPath + File.separator + REQUIREMENT);
-        }else if(type.contains("java")){
-             target = Paths.get(HomeGUI.projectPath + File.separator + SOURCE_CODE);
-        }else if(type.contains("mdj")||type.contains("json")||type.contains("xmi")||type.contains("uml")){
-             target = Paths.get(HomeGUI.projectPath + File.separator + UML);
-        }else{
-            target = Paths.get(HomeGUI.projectPath + File.separator + PROPERTY);
-        }
-        return target;
-    }
+    public static final String IMAGE_PATH = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Resources"+File.separator + 
+			            "Images" + File.separator;
+//    public static Path getPath(Path target,String type){
+//        if(type.contains(XML)){
+//            target = Paths.get(HomeGUI.projectPath + File.separator + XML);
+//        }else if(type.contains(REQUIREMENT) || type.contains("txt") || type.contains("pdf")||
+//                type.contains("docs")||type.contains("doc")){
+//            target = Paths.get(HomeGUI.projectPath + File.separator + REQUIREMENT);
+//        }else if(type.contains("java")){
+//             target = Paths.get(HomeGUI.projectPath + File.separator + SOURCE_CODE);
+//        }else if(type.contains("mdj")||type.contains("json")||type.contains("xmi")||type.contains("uml")){
+//             target = Paths.get(HomeGUI.projectPath + File.separator + UML);
+//        }else{
+//            target = Paths.get(HomeGUI.projectPath + File.separator + PROPERTY);
+//        }
+//        return target;
+//    }
     public static void addSubFolderIntoProject(File folder){
         
         if(!folder.exists())
@@ -168,5 +163,17 @@ public class FilePropertyName {
 			        	        System.out.println("File copied from " + src + " to " + dest);
 			        	}
 			        }
+    	
+    	public static void copyFile(File sourceFile, File destFile) throws IOException {
+    			
+    			String srcFilePath = sourceFile.getAbsolutePath();
+    			
+    			int startIndex = srcFilePath.lastIndexOf("/");
+    			int endIndex = srcFilePath.length();
+    			String fileName = srcFilePath.substring(startIndex+1, endIndex);
+    			
+    			destFile = new File(destFile.getAbsolutePath(),fileName);
+    			copyFolder(sourceFile, destFile);
+    	}
 }
 
