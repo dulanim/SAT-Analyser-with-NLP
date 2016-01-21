f88a66d72e9af494b4966c81b680bff5c7c90741
shiyamalan <rsrishiya@gmail.com>
2016-01-21 11:01:34 +0530

    shiyam changed StaticData.java

--- src/main/java/com/project/traceability/ontology/models/StaticData.java
+++ src/main/java/com/project/traceability/ontology/models/StaticData.java
@@ -1,5 +1,8 @@
 
 package com.project.traceability.ontology.models;
+
+import com.project.traceability.GUI.WordExpWindows;
+
 public class StaticData {
 
 	public static final String OWL_ROOT_URI = "http://www.sat.analyser.com/words/";
@@ -21,4 +24,8 @@ public class StaticData {
 	public static final String OWL_FILE_FORMAT = "RDF/XML-ABBREV";
 	public static final String OWL_PROPERTY = "";
 	
+        public static final String OWL_INFOS = "If you add both words"
+                +WordExpWindows.word1+ " & " + WordExpWindows.word2 +", "+
+                " you do not need to add for futher projects\n\n"+
+                "Do you want to add to Dictionary those words?";
 }
