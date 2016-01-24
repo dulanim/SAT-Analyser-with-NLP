1afba45023334e530c5586434d76ebb7f8062984
shiyamalan <rsrishiya@gmail.com>
2016-01-24 22:53:53 +0530

    changed Creation plan by shiyam

--- src/main/java/com/project/traceability/ontology/models/ModelCreator.java
+++ src/main/java/com/project/traceability/ontology/models/ModelCreator.java
@@ -45,6 +45,9 @@ public class ModelCreator {
 		 * this method is for initiating the .owl file that 
 		 * which is internally created by system when ontology model requires once
          */
+    	File f = new File(filePath + File.separator +  filename);
+    	if(f.exists())
+    		return true;
         PrintWriter writer;
         isCreated = false;
         try {
@@ -118,105 +121,129 @@ public class ModelCreator {
             // Create an empty model
             final Model base = ModelFactory.createDefaultModel();
             model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF, base);
+            initModel();
         }
         return model;
     }
 
-    @SuppressWarnings("finally")
     public boolean createNewNode(String word1, String word2, Word word) {
-        boolean isCreated = false;
+    	  boolean isCreated = false;
 
-        String wordURI1 = StaticData.OWL_ROOT_URI + "#" + word1;
-        String wordURI2 = StaticData.OWL_ROOT_URI + "#" + word2;
+//    	  word1 = getNormalizedWordString(word1);
+//    	  word2 = getNormalizedWordString(word2);
+          String wordURI1 = StaticData.OWL_ROOT_URI + "#" + word1;
+          String wordURI2 = StaticData.OWL_ROOT_URI + "#" + word2;
 
-        /*
-		 * get the word's root/parent
-		 * get the word's property name
-		 * get the word's literal / value
-         */
-        String aParent = word.getParentName();
-        List<String> aLiteral = word.getValue();
-        List<String> aProperty = word.getPropertyName();
+          /*
+  		 * get the word's root/parent
+  		 * get the word's property name
+  		 * get the word's literal / value
+           */
+          
+          List<String> aParent = word.getParentName();
+          List<String> aLiteral = word.getValue();
+          List<String> aProperty = word.getPropertyName();
 
-        OntModel model = this.getCreatedModel();
-        OntClass class1 = null;// =	model.createClass(wordURI1);
-        OntClass class2 = null;// = 	model.createClass(wordURI2);
-        //get the root position
-        String wordURI = StaticData.OWL_ROOT_URI + "#" + aParent;
-        OntClass rootClass = this.model.getOntClass(wordURI);
-        if (rootClass == null) {
-            /*
-			 * this block works when parent class other than existing model
-			 * Animal is not there in model 
-			 * so we get this as  Class 
-			 * so add Animal under Class
-			 * now animal is top or parent class
-             */
-            String type = word.getType();
-            String uri = StaticData.OWL_ROOT_URI + "#" + aParent;
-            wordURI = StaticData.OWL_ROOT_URI + "#" + type;
-            rootClass = this.model.getOntClass(wordURI);
-            OntClass newClass = this.model.createClass(uri);
-            rootClass.addSubClass(newClass);
-            rootClass = newClass;
-        }
-        // Read the RDF/XML file
-        model.read(getInputStream(), null);
-        boolean isClass1Exists = isExistis(wordURI1);
-        boolean isClass2Exists = isExistis(wordURI2);
-
-        if (isClass2Exists || isClass1Exists) {
-            //if class 1 is not there in model
-            if (isClass2Exists) {
-                class1 = model.createClass(wordURI1);
-                rootClass.addSubClass(class1);
-                for (int i = 0; i < aProperty.size(); i++) {
-                    OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
-                            + StaticData.OWL_PROPERTY + aProperty.get(i));
-                    class1.addProperty(relation, aLiteral.get(i));
-                }
-            }
-            //if class 2 is not there in model
-            if (isClass1Exists) {
-                class2 = model.createClass(wordURI2);
-                rootClass.addSubClass(class2);
-                for (int i = 0; i < aProperty.size(); i++) {
-                    OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
-                            + StaticData.OWL_PROPERTY + aProperty.get(i));
-
-                    class2.addProperty(relation, aLiteral.get(i));
-                }
-            }
-        }
-
-        // if two class is not in model already add those two node
-        if (!(isClass1Exists && isClass2Exists) && (!aParent.equals(null)
-                || !aParent.isEmpty() || !aParent.equals(""))) {
-            class1 = model.createClass(wordURI1);
-            class2 = model.createClass(wordURI2);
-            rootClass.addSubClass(class1);
-            rootClass.addSubClass(class2);
-            for (int i = 0; i < aProperty.size(); i++) {
-                OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
-                        + StaticData.OWL_PROPERTY + aProperty.get(i));
-                class1.addProperty(relation, aLiteral.get(i));
-                class2.addProperty(relation, aLiteral.get(i));
-            }
-        }
-        try {
-            //update or write to owl file 
-            model.write(getOWLWriter(), StaticData.OWL_FILE_FORMAT);
-            isCreated = true;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            //send notification back to caller to success or not 
-            return isCreated;
-        }
+          for(String parent:aParent){
+	          OntModel model = getCreatedModel();
+	          OntClass class1 = null;// =	model.createClass(wordURI1);
+	          OntClass class2 = null;// = 	model.createClass(wordURI2);
+	          //get the root position
+	          String wordURI = StaticData.OWL_ROOT_URI + "#" + parent;
+	          OntClass rootClass = this.model.getOntClass(wordURI);
+	          if (rootClass == null && parent != null && !parent.isEmpty()) {
+	              /*
+	  			 * this block works when parent class other than existing model
+	  			 * Animal is not there in model 
+	  			 * so we get this as  Class 
+	  			 * so add Animal under Class
+	  			 * now animal is top or parent class
+	               */
+	              String type = word.getType();
+	              String uri = StaticData.OWL_ROOT_URI + "#" + parent;
+	              wordURI = StaticData.OWL_ROOT_URI + "#" + type;
+	              rootClass = model.getOntClass(wordURI);
+	              OntClass newClass = model.createClass(uri);
+	              rootClass.addSubClass(newClass);
+	              rootClass = newClass;
+	          }else if(parent == "" || parent == null){
+	        	  String type = word.getType();
+	              wordURI = StaticData.OWL_ROOT_URI + "#" + type;
+	              rootClass = model.getOntClass(wordURI);
+	          }
+	          // Read the RDF/XML file
+	          model.read(getInputStream(), null);
+	          boolean isClass1Exists = isExistis(wordURI1);
+	          boolean isClass2Exists = isExistis(wordURI2);
+	
+	          if (isClass2Exists || isClass1Exists) {
+	              //if class 1 is not there in model
+	              if (isClass2Exists) {
+	                  class1 = model.createClass(wordURI1);
+	                  class2 = model.getOntClass(wordURI2);
+	                  rootClass.addSubClass(class1);
+	                  rootClass.addSubClass(class2);
+	                  for (int i = 0; i < aProperty.size(); i++) {
+	                      OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
+	                              + StaticData.OWL_PROPERTY + aProperty.get(i));
+	                      class1.addProperty(relation, aLiteral.get(i));
+	                      class2.addProperty(relation, aLiteral.get(i));
+	                  }
+	              }
+	              //if class 2 is not there in model
+	              if (isClass1Exists) {
+	                  class2 = model.createClass(wordURI2);
+	                  class1 = model.createClass(wordURI1);
+	                  rootClass.addSubClass(class2);
+	                  rootClass.addSubClass(class1);
+	                  for (int i = 0; i < aProperty.size(); i++) {
+	                      OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
+	                              + StaticData.OWL_PROPERTY + aProperty.get(i));
+	
+	                      class1.addProperty(relation, aLiteral.get(i));
+	                      class2.addProperty(relation, aLiteral.get(i));
+	                  }
+	              }
+	          }
+	
+	          // if two class is not in model already add those two node
+	          if (!(isClass1Exists && isClass2Exists) && (!parent.equals(null)
+	                  || !parent.isEmpty() || !parent.equals(""))) {
+	              class1 = model.createClass(wordURI1);
+	              class2 = model.createClass(wordURI2);
+	              
+	              if(rootClass != null){
+		              rootClass.addSubClass(class1);
+		              rootClass.addSubClass(class2);
+	              }
+	              for (int i = 0; i < aProperty.size(); i++) {
+	                  OntProperty relation = model.createOntProperty(StaticData.OWL_ROOT_URI + "#"
+	                          + StaticData.OWL_PROPERTY + aProperty.get(i));
+	                  class1.addProperty(relation, aLiteral.get(i));
+	                  class2.addProperty(relation, aLiteral.get(i));
+	              }
+	          }
+	          
+	          if(isClass1Exists && isClass2Exists){
+	        	  // both class exist is model add parent classes to existing model
+	        	  class1 = model.getOntClass(wordURI1);
+	        	  class2 = model.getOntClass(wordURI2);
+	        	  rootClass.addSubClass(class1);
+	              rootClass.addSubClass(class2);
+	          }
+	          try {
+	              //update or write to owl file 
+	              model.write(getOWLWriter(), StaticData.OWL_FILE_FORMAT);
+	              isCreated = true;
+	          } catch (FileNotFoundException e) {
+	              e.printStackTrace();
+	          } catch (IOException e) {
+	              e.printStackTrace();
+	          } catch (Exception e) {
+	              e.printStackTrace();
+	          }
+         }
+         return isCreated;
     }
 
     public boolean isMatchingWords(String word1, String word2) {
@@ -225,19 +252,29 @@ public class ModelCreator {
         double score = 0;
         double total = 1;
         if (!(word1.equals("") && word2.equals(""))) {
+        	
+//        	word1 = getNormalizedWordString(word1);
+//        	word2 = getNormalizedWordString(word2);
             String uri1 = StaticData.OWL_ROOT_URI + "#" + word1;
             String uri2 = StaticData.OWL_ROOT_URI + "#" + word2;
 
             OntModel model = this.getCreatedModel();
             InputStream in = this.getInputStream();
-            model.read(in, null);
-            OntClass parent1 = model.getOntClass(uri1);
-            OntClass parent2 = model.getOntClass(uri2);
-
-            if (parent1 == null || parent2 == null) {
-
-                return false;
+            model.read(in, null);///read the model.owl file from hard disk
+            
+            NavigationModel navigationModel = NavigationModel.getNavigatorInstane();
+            OntClass wordlass1 = model.getOntClass(uri1);
+            OntClass wordlass2 = model.getOntClass(uri2);
+            
+            if (wordlass1 == null || wordlass2 == null) {
+        		//if class is not in model they should not match with each other
+            	return false;
             }
+            //gather parent classes for separate word1 and word classes
+            List<String> parent1 = navigationModel.getParentClassFor(wordlass1);
+            List<String> parent2 = navigationModel.getParentClassFor(wordlass2);
+
+            
             NavigationModel navigator = NavigationModel.getNavigatorInstane();
             Map<String, List<String>> map = navigator.getAllClassesProperty();
 
@@ -275,10 +312,29 @@ public class ModelCreator {
                 score = (weight / total);
             }
 
-            if (parent1.getLocalName().equalsIgnoreCase(parent2.getLocalName())) {
-                double newTotal = size1 + size2 + 1;
-                score += (weight + 1) / (newTotal);
+            if(parent1.size()>=parent2.size()){
+            	//parent1's size more check all parent in parent2 is in parent1
+            	//increase the weight by 1
+            		for(String parent:parent2){
+            			if(parent1.contains(parent)){
+            				weight += 1;
+            			}
+            		}
+            		 double newTotal = total + parent1.size();
+                     score = weight / (newTotal);
+                
+            }else{
+            	//parent2's size more check all parent in parent1 is in parent2
+            	//increase the weight by 1
+            	for(String parent:parent1){
+        			if(parent2.contains(parent)){
+        				weight += 1;
+        			}
+        		}
+        		 double newTotal = total + parent2.size();
+                 score = weight / (newTotal);
             }
+            
         }
 
         if (score >= 0.7 && score <= 1.0) {
@@ -291,7 +347,7 @@ public class ModelCreator {
 
     private PrintWriter getOWLWriter() throws FileNotFoundException, IOException, Exception {
 
-        FileOutputStream fos = new FileOutputStream(this.filePath
+        FileOutputStream fos = new FileOutputStream(this.filePath + File.separator
                 + filename, false);
         PrintWriter writer = new PrintWriter(fos);
         return writer;
@@ -307,6 +363,7 @@ public class ModelCreator {
 
             if (uriClass.equals(tempClassURI)) {
                 parentClass = tempClass;
+                isExists = true;
                 return isExists;
             }
 
@@ -316,13 +373,35 @@ public class ModelCreator {
 
     public InputStream getInputStream() {
         // Use the FileManager to find the input file
-        filePath = System.getProperty("user.dir")+File.separator+"res"; 
+        //filePath = System.getProperty("user.dir")+File.separator+"res"; 
         InputStream in = FileManager.get().open(filePath + File.separator + filename);
         if (in == null) {
             throw new IllegalArgumentException("File: " + filename + " not found");
         }
         return in;
 
+    }
+    
+    public String getNormalizedWordString(String word){
+    	//all name in model should be in lower case it is our convention for avoiding conflict
+    	String norm = "";
+    	if(word.length() ==1){
+	    	char chfirst = word.charAt(0);
+	    	String str = new String(new char[]{chfirst});
+	    	str.toUpperCase();
+	    	norm = str;
+    	}else if(word.length()>1){
+    		char chfirst = word.charAt(0);
+	    	String str = new String(new char[]{chfirst});
+	    	str.toUpperCase();
+	    	
+	    	String sub = word.substring(1);
+	    	str += sub;
+	    	norm =str;
+    	}else{
+    		norm = "";
+    	}
+    	return norm;
     }
 
 }
