89d35fcda2f9cdf9c7ca72f7c723136245de8334
shobi22 <shobi.11@cse.mrt.ac.lk>
2016-02-01 01:39:14 +0530

    NLPRequirementMain: modified

--- src/main/java/com/project/NLP/Requirement/NLPRequirementMain.java
+++ src/main/java/com/project/NLP/Requirement/NLPRequirementMain.java
@@ -22,9 +22,9 @@ import java.util.Iterator;
 
 public class NLPRequirementMain {
 
-    private static final String REQUIREMENT_INPUT_FILE = "io/Order_requirement.txt"; // input file
+    private static final String REQUIREMENT_INPUT_FILE = "io/Hostel_Management_Requirement.txt"; // input file
     private static String requirementDocument = ""; //variable to hold the input document 
-    private static HashMap requirementObjects = new HashMap(); // to store the final artefacts in the map
+    public static HashMap requirementObjects = new HashMap(); // to store the final artefacts in the map
     private static HashSet<ClassRelation> requirementObjectRelations = new HashSet<>();// to store the final relationships in the map
 
     public static void extractRequirement() {
@@ -291,7 +291,7 @@ public class NLPRequirementMain {
 
                
             //System.out.println(sbf.toString());
-            BufferedWriter bwr = new BufferedWriter(new FileWriter("io/Requirement_Output_Order_Machine.txt"));
+            BufferedWriter bwr = new BufferedWriter(new FileWriter("io/Requirement_Output_Hostel_Machine.txt"));
             /*write contents of StringBuffer to a file*/
             bwr.write(sbf.toString());
             /*flush the stream*/
